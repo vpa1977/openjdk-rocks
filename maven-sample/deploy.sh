de38a861773f67f242e577bde70f8eb635ba1493
Vladimir Petko <vladimir.petko@canonical.com>
2024-08-16 18:20:06 +1200

    fix: remove extension in the find command

--- maven-sample/deploy.sh
+++ maven-sample/deploy.sh
@@ -4,5 +4,5 @@ name=$1
 (cp -r sample/ ${1}
     cd ${1} &&
     rockcraft pack &&
-    find . -name *.rock -exec sudo skopeo --insecure-policy copy oci-archive:{}.rock docker-daemon:${1}:latest \;
+    find . -name *.rock -exec sudo skopeo --insecure-policy copy oci-archive:{} docker-daemon:${1}:latest \;
     )
